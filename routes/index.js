8040d4edaaca5458fd2f0a829ca100b17892759d
Eunice Lin <eunicel@mit.edu>
2015-06-27 18:37:56 -0500

    fixed a typo in routes/index.js

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var request require('request');
+var request = require('request');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
