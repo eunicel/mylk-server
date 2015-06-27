2060bea073ac656a07151830df65c59b0a52ef8f
Eunice Lin <eunicel@mit.edu>
2015-06-27 18:35:26 -0500

    added process.env.PORT to app.js and route for payments

--- app.js
+++ app.js
@@ -56,7 +56,7 @@ app.use(function(err, req, res, next) {
   });
 });
 
-app.listen(3000);
+app.listen(process.env.PORT || 3000);
 
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,29 @@
 var express = require('express');
 var router = express.Router();
+var request require('request');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/venmo_oauth', function(req, res) {
+  request.post(
+    'https://api.venmo.com/v1/payments',
+    {
+      form: {
+      access_token: req.body.access_token,
+      user_id: req.body.user_id,
+      amount: 5,
+      note: "donation made through mylk"
+      }
+    },
+    function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            console.log(body)
+        }
+    }
+  );
 });
 
 module.exports = router;
