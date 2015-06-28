18c38c358798a468c9591b9761c83cfc5baa86eb
Dong Hyun Choi <andy.dh.choi@gmail.com>
2015-06-28 00:38:38 -0500

    add api for JustGiving

--- routes/index.js
+++ routes/index.js
@@ -12,6 +12,34 @@ router.get('/venmo_oauth', function(req, res) {
   res.render('index', { title: "Venmo Redirect", access_token: req.query.access_token, user_id: "145434160922624933" });
 });
 
+/* response with charity recommendation from JustGiving */
+router.get('/get-charity-recommendation', function(req, res) {
+  var appId = 'dc91d19d';
+  var category = req.query.category;
+
+  request.get({
+    url: 'https://api.justgiving.com/'+appId+'/v1/charity/search?q='+category,
+    headers: {
+      'Accept': 'application/json'
+    }
+  }, function(error, response, body) {
+    var queryResult = JSON.parse(body);
+    var firstMatch = queryResult.charitySearchResults[0];
+    request.get({
+      url: 'https://api.justgiving.com/'+appId+'/v1/charity/'+firstMatch.charityId,
+      headers: {
+        'Accept': 'application/json'
+      }
+    }, function(error, response, body) {
+      res.json({
+        name: firstMatch.name,
+        description: firstMatch.description,
+        url: JSON.parse(body).profilePageUrl
+      });
+    });
+  });
+});
+
 router.post('/donate', function(req, res) {
   request.post(
     'https://sandbox-api.venmo.com/v1/payments',
