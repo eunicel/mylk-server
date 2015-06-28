d80ec5f4edbc0d91a2eecb34692aef2e3a24b1d8
gwcmk <gregwcmkim@gmail.com>
2015-06-27 20:02:56 -0500

    Got donations working with sandbox

--- routes/index.js
+++ routes/index.js
@@ -8,23 +8,27 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/venmo_oauth', function(req, res) {
-  res.render('index', { access_token: req.query.access_token, user_id: "Eunice-Lin" });
+  res.render('index', { access_token: req.query.access_token, user_id: "145434160922624933" });
 });
 
 router.post('/donate', function(req, res) {
   request.post(
-    'https://api.venmo.com/v1/payments',
+    'https://sandbox-api.venmo.com/v1/payments',
     {
       form: {
       access_token: req.body.access_token,
       user_id: req.body.user_id,
-      amount: 5,
+      amount: 0.10,
       note: "donation made through mylk"
       }
     },
     function (error, response, body) {
         if (!error && response.statusCode == 200) {
-            console.log(body)
+          console.log(body);
+          res.send("success");
+        } else {
+          console.log(body);
+          res.send("fail");
         }
     }
   );
